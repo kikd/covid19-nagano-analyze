fd1343f44ae2bd42f429b8792c1b46aac30247d1
Kohei Ikeda <tdn.suama@gmail.com>
2021-08-09 12:03:51 +0900

    * 公表日ベースの陽性者数を取得する日付の計算方法変更(検査状況が休日更新されないため) * 簡易実効再生産数の計算を追加

--- scripts/datawrangle.m
+++ scripts/datawrangle.m
@@ -7,18 +7,39 @@ patients = getPatients;
 
 %% 公表日ベースの陽性者数取得
 % 0人だった日も知りたいので、日付は検査状況から引用する
-d = test_count.InspectionDate;
+% -> 休日は検査状況が更新されていないことを考えていなかった。。。
+% -> 発生状況の日付も見たほうがよい。
+% -> 自動更新ができるようになったらyesterdayを最後の日付にしてもいいかも。
+start_date = test_count.InspectionDate(1);
+end_date = max(test_count.InspectionDate(end), patients.ConfirmedDate(end));
+d = [start_date:end_date]';
 confirmedNumberbyDate = zeros(numel(d),1);
 for index = 1:numel(d)
-confirmed_number = length(find(patients.ConfirmedDate==d(index)));
-confirmedNumberbyDate(index) = confirmed_number;
+    before_generation = max(1, index - 5);
+    confirmed_number = length(find(patients.ConfirmedDate==d(index)));
+    confirmedNumberbyDate(index) = confirmed_number;
 end
 
 %% 各7日間移動平均を計算
 % 公表日ベースの陽性者数
 confirmednumber_movave = movmean(confirmedNumberbyDate,[6 0]);
 
-% 検査数
+%% 簡易実効再生産数の計算
+% 7日間移動平均を用いて計算する
+% 東洋経済オンラインと同じ数式とする
+rt0 = zeros(numel(d),1);
+for index = 1:numel(d)
+    before_generation = max(1, index - 7);
+    beforeval = confirmednumber_movave(before_generation);
+    % 0除算対策
+    if beforeval == 0
+        beforeval = 1;
+    end
+    rt0(index) = (confirmednumber_movave(index) / beforeval) ^ (5/7);
+end
+
+
+%% 検査数
 inspection_movave = movmean(test_count.InspectionNum, [6 0]);
 % 検査日ベースの陽性者数
 positive_movave = movmean(test_count.Positive, [6 0]);
