6ff70c2dac72d7866f8e4bfbcc61d25fc73d05d2
Kohei Ikeda <tdn.suama@gmail.com>
2021-08-12 23:29:15 +0900

    jsonファイルの出力を追加

--- scripts/datawrangle.m
+++ scripts/datawrangle.m
@@ -66,11 +66,3 @@ positive_movave = movmean(test_count.Positive, [6 0]);
 positive_rate = test_count.Positive ./ test_count.InspectionNum .* 100;
 posrate_movave = movmean(positive_rate, [6 0]);
 posrate_movave2 = positive_movave./inspection_movave.*100;
-
-function makejson(data, filename, update_time)
-jsondata = struct();
-jsondata.lastUpdated = update_time;
-
-
-end
-
