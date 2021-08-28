d2bba2c3c4182f95195e5976af8ba76d63948201
Kohei Ikeda <tdn.suama@gmail.com>
2021-08-28 21:04:28 +0900

    CSVのURLをスクレイピングで取得するように変更 もうちょっといい方法があるか同時に考える

--- scripts/getPatients.m
+++ scripts/getPatients.m
@@ -1,6 +1,6 @@
-function [patients, updated] = getPatients()
+function [patients, updated] = getPatients(url)
     %% ホームページからCSVファイルを取得
-    websave('csv/200000_nagano_covid19_patients.csv','https://www.pref.nagano.lg.jp/hoken-shippei/kenko/kenko/kansensho/joho/documents/200000_nagano_covid19_patients.csv');
+    websave('csv/200000_nagano_covid19_patients.csv', url);
 
     %% インポート オプションの設定およびデータのインポート
     opts = delimitedTextImportOptions("NumVariables", 15);
